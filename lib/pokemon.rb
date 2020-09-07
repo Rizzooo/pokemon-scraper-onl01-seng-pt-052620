73bdbfbe776bcb2eb03b6e4a2be0c1a414f9cbc5
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-09-07 18:28:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,10 @@
 class Pokemon
+<<<<<<< HEAD
   attr_accessor :name, :type, :db, :id
+=======
+  attr_accessor :name, :type
+  attr_reader :id
+>>>>>>> ccee5079b196fa8cd214c10e739f5ec31e9bc94e
   
   def initialize(name:, type:, db:, id:)
     @name = name 
@@ -8,6 +13,7 @@ class Pokemon
     @id = id
   end
   
+<<<<<<< HEAD
   def self.save(name, type, db)
     prepared = db.prepare(" INSERT INTO pokemon (name, type)
         VALUES (?, ?)")
@@ -24,5 +30,15 @@ class Pokemon
       pokemon
     end
     result[0]
+=======
+  def self.save
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+      SQL
+      
+    @db.execute(sql, self.name, self.type)
+    @id = @db.execute("SELECT last_insert_rowid() from pokemon")[0][0]
+>>>>>>> ccee5079b196fa8cd214c10e739f5ec31e9bc94e
   end
 end
